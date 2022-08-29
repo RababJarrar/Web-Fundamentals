298e6228cf5b08194d433d5d6b06e5b631fc8ec8
RababJarrar <rababjarrar1997@yahoo.com>
2022-08-29 13:00:17 +0300

    modify

---
+++ JS/Loop Challenges/ass.js
@@ -0,0 +1,29 @@
+for ( var i=1; i<20 ; i=i+2){
+    console.log(i);  
+}
+
+ console.log("------------------------------------------------");
+ for ( var i=100; i>-1 ; i--){
+    if( i%3==0){
+    console.log(i);
+    }  
+}
+
+console.log("------------------------------------------------");
+for ( var i=4; i>-5 ; i=i-1.5){
+   console.log(i);
+    } 
+
+console.log("------------------------------------------------");
+var sum=0;
+for ( var i=1; i<101 ; i++){
+   sum+=i;
+    }  
+    console.log( sum);
+
+console.log("------------------------------------------------");
+    var sum2=1;
+    for ( var i=1; i<13 ; i++){
+       sum2*=i;
+        }  
+    console.log(sum2);
