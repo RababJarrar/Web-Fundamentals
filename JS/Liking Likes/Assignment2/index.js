62a933191c59fed35a1d9e07140c2cbff4ffa31a
RababJarrar <rababjarrar1997@yahoo.com>
2022-08-30 20:55:50 +0300

    modify

---
+++ JS/Liking Likes/Assignment2/index.js
@@ -0,0 +1,30 @@
+var count =9;
+    var countElement = document.querySelector("#count");
+    console.log(countElement); 
+     
+    function add() {
+        count ++;
+        countElement.innerText = count +" like(s)";
+        console.log(count);    
+    }
+
+ var count2 =12;
+ var count2Element = document.querySelector("#count2");
+console.log(count2Element); 
+     
+ function addc2() {
+ count2 ++;
+ count2Element.innerText = count2 +" like(s)";
+ console.log(count2);    
+  }
+
+  var count3 =9;
+ var count3Element = document.querySelector("#count3");
+console.log(count3Element); 
+     
+ function addc3() {
+ count3 ++;
+ count3Element.innerText = count3 +" like(s)";
+ console.log(count3);    
+  }
+   
