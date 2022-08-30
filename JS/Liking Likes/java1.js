346f67e44489c46444936deded10e9145bc2080d
RababJarrar <rababjarrar1997@yahoo.com>
2022-08-30 16:49:56 +0300

    modify

--- JS/Liking Likes/java1.js
+++ JS/Liking Likes/java1.js
@@ -4,7 +4,6 @@
     console.log(countElement); 
      
     function add1() {
-        console.log(count); 
         count ++;
         countElement.innerText = count +" like(s)";
         console.log(count);    
