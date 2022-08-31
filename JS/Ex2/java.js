51bc1c8051858980a3e6d6563769778077d7e381
RababJarrar <rababjarrar1997@yahoo.com>
2022-08-31 13:13:37 +0300

    modify

---
+++ JS/Ex2/java.js
@@ -0,0 +1,8 @@
+function code(){
+    for (var i=0; i<10; i++){
+        console.log(i);
+    }
+}
+console.log("HI");
+setTimeout(code,5000);
+console.log("Bye");
---
+++ JS/Profile Page-JS/script.js
@@ -0,0 +1,17 @@
+console.log("page loaded...");
+var changename = document.querySelector("#name");
+function change(){
+    changename.innerText = "Rabab" ;
+}
+
+
+var requestname1 = document.querySelector("#remove1");
+function re1(){
+    requestname1.remove();
+}
+
+var requestname2 = document.querySelector("#remove2");
+function re2(){
+    requestname2.remove();
+}
+
