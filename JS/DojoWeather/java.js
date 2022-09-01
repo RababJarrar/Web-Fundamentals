1b3b00153476b9bde62cc8aa4f8e734de02ecb18
RababJarrar <rababjarrar1997@yahoo.com>
2022-09-01 08:29:36 +0300

    initial

---
+++ JS/DojoWeather/java.js
@@ -0,0 +1,25 @@
+function city(element){
+    alert ("Loading weather report...");
+}
+function di(){
+    var message= document.querySelector("#cookiemessage");
+    message.remove();
+}
+function F(element){
+    var fahrenhite1= document.querySelector("#numbers1");
+    var fahrenhite2= document.querySelector("#numbers2");
+    var fahrenhite3= document.querySelector("#numbers3");
+    var fahrenhite4= document.querySelector("#numbers4");
+    var fahrenhite5= document.querySelector("#numbers5");
+    var fahrenhite6= document.querySelector("#numbers6");
+    var fahrenhite7= document.querySelector("#numbers7");
+    var fahrenhite8= document.querySelector("#numbers8");
+    fahrenhite1.innerText=75;
+    fahrenhite2.innerText=80;
+    fahrenhite3.innerText=69;
+    fahrenhite4.innerText=78;
+    fahrenhite5.innerText=65;
+    fahrenhite6.innerText=66;
+    fahrenhite7.innerText=61;
+    fahrenhite8.innerText=70;
+}
