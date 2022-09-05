37ff21a46d531a3e7731d8386da617e065d5e45d
RababJarrar <rababjarrar1997@yahoo.com>
2022-09-05 15:47:50 +0300

    modify

--- JS/DojoWeather/java.js
+++ JS/DojoWeather/java.js
@@ -5,21 +5,27 @@ function di(){
     var message= document.querySelector("#cookiemessage");
     message.remove();
 }
-function F(element){
-    var fahrenhite1= document.querySelector("#numbers1");
-    var fahrenhite2= document.querySelector("#numbers2");
-    var fahrenhite3= document.querySelector("#numbers3");
-    var fahrenhite4= document.querySelector("#numbers4");
-    var fahrenhite5= document.querySelector("#numbers5");
-    var fahrenhite6= document.querySelector("#numbers6");
-    var fahrenhite7= document.querySelector("#numbers7");
-    var fahrenhite8= document.querySelector("#numbers8");
-    fahrenhite1.innerText=75+"°";
-    fahrenhite2.innerText=80+"°";
-    fahrenhite3.innerText=69+"°";
-    fahrenhite4.innerText=78+"°";
-    fahrenhite5.innerText=65+"°";
-    fahrenhite6.innerText=66+"°";
-    fahrenhite7.innerText=61+"°";
-    fahrenhite8.innerText=70+"°";
-}
+
+                            
+function temp(x){          //temp(value)
+                           //console.log(x)
+      if(x=="feh"){
+            for (var i=1; i<9; i++){
+                var c= document.querySelector("#number"+i)
+                           // console.log(c)
+                c.innerText=( Math.round((c.innerText)*9/5)+32)
+            }  
+      }
+    else{
+            for (var i=1; i<9; i++){
+              var f= document.querySelector("#number"+i)
+                          //console.log(f)
+              f.innerText=( Math.round(((f.innerText)-32)*(5/9)))
+            }
+        }
+  }
+
+
+
+
+    
