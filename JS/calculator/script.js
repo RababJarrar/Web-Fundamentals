39888ebf7c9c309c2807df84d9938cb2c0b9118d
RababJarrar <rababjarrar1997@yahoo.com>
2022-09-06 00:09:02 +0300

    modify

--- JS/calculator/script.js
+++ JS/calculator/script.js
@@ -1,13 +1,50 @@
-var element = document.getElementById("display");
-
-function press(content){
-    if (element.innerText==0){
-        element.innerText="" 
-    }
-    element.innerText+=content
-}
+var display = document.getElementById("display");
+          //console.log(display.innerText);
+var number1 ="";
+var number2;
+var op;
+var result ; 
 
 function clr(){
-    element.innerText=0
-    
+    display.innerText=0 
+     number1 =""
+     number2 ="" 
+    op =""
+}
+
+function press(number){
+    if (display.innerText==0){
+        display.innerText="" 
+    }
+    number1=number1+number
+    display.innerText=number1
+        //console.log(number1)   
+}
+
+function setOP(key){
+    op=key
+       //console.log(op)
+    number2=number1
+    console.log(number2)
+    number1=""
+    console.log(number1)
+}
+
+
+function calculate(){
+    var a= parseFloat(number2)
+    var b= parseFloat(number1)
+   if(op=='/'){
+        result=a/b
+    }
+    if(op=='+'){
+        result=a+b
+    }
+    if(op=='-'){
+        result=a-b
+    }
+    if(op=='*'){
+        result=a*b
+    }
+   display.innerText=result
 }
