54384954317e61b5bb10d7da5a929ce5e54f453e
RababJarrar <rababjarrar1997@yahoo.com>
2022-09-06 10:48:17 +0300

    initial

---
+++ JS/test/test.js
@@ -0,0 +1,28 @@
+var str1 = "Hello Coding Dojo"
+var str2= "AXSOS"
+result = str1.concat(" and Axsoo")
+console.log(result)
+result1 = str1.concat(" ",str2," ","and people")
+console.log(result1)
+
+result2 = str2.toLowerCase()
+console.log(result2)
+
+result3 = str1.slice(6,10)
+console.log(result3)
+result4 = str1.slice(-7,-1)
+console.log(result4)
+result5 = str1.slice(0,5)
+console.log(result5)
+
+result6 = str1.substring(0,7)
+console.log(result6)
+
+result7= str1.charAt(6)
+console.log(result7)
+
+result8= str1.split(" ")
+console.log(result8)
+
+
+
